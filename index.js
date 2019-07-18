e7ea224a304e5d097b9c009b144710c9ef0993b0
Sean Glancy <spglancy@gmail.com>
2019-07-18 14:13:51 -0700

    coveralls + travis

--- index.js
+++ index.js
@@ -1,94 +1,84 @@
-const toCaps = string => {
-  return string[0].toUpperCase() + string.slice(1, string.length)
-}
+const toCaps = string => string[0].toUpperCase() + string.slice(1, string.length);
 
-String.prototype.capitalize = function() {
-  return this[0].toUpperCase() + this.slice(1, this.length)
-}
+String.prototype.capitalize = function () {
+  return this[0].toUpperCase() + this.slice(1, this.length);
+};
 
-String.prototype.lower = function() {
-  return this[0].toLowerCase() + this.slice(1, this.length)
-}
+String.prototype.lower = function () {
+  return this[0].toLowerCase() + this.slice(1, this.length);
+};
 
-const sentenceCaps = string => {
-  return string
-    .split(" ")
+const sentenceCaps = string => string
+  .split(' ')
+  .map(word => toCaps(word))
+  .join(' ');
+
+String.prototype.capitalizeSentence = function () {
+  return this.split(' ')
     .map(word => toCaps(word))
-    .join(" ")
-}
+    .join(' ');
+};
 
-String.prototype.capitalizeSentence = function() {
-  return this.split(" ")
-    .map(word => toCaps(word))
-    .join(" ")
-}
+const everyOtherCaps = string => string
+  .split('')
+  .map((letter, i) => (i % 2 === 0 ? letter.toUpperCase() : letter))
+  .join('');
 
-const everyOtherCaps = string => {
-  return string
-    .split("")
+String.prototype.evenCaps = function () {
+  return this.split('')
     .map((letter, i) => (i % 2 === 0 ? letter.toUpperCase() : letter))
-    .join("")
-}
+    .join('');
+};
 
-String.prototype.evenCaps = function() {
-  return this.split("")
-    .map((letter, i) => (i % 2 === 0 ? letter.toUpperCase() : letter))
-    .join("")
-}
-
-String.prototype.oddCaps = function() {
-  return this.split("")
+String.prototype.oddCaps = function () {
+  return this.split('')
     .map((letter, i) => (i % 2 === 1 ? letter.toUpperCase() : letter))
-    .join("")
-}
+    .join('');
+};
 
-const removeWhitespace = string => {
-  return string
-    .split("")
-    .map(letter => (letter === " " ? "" : letter))
-    .join("")
-}
+const removeWhitespace = string => string
+  .split('')
+  .map(letter => (letter === ' ' ? '' : letter))
+  .join('');
 
-String.prototype.removeWhitespace = function() {
-  return this.split("")
-    .map(letter => (letter === " " ? "" : letter))
-    .join("")
-}
+String.prototype.removeWhitespace = function () {
+  return this.split('')
+    .map(letter => (letter === ' ' ? '' : letter))
+    .join('');
+};
 
-const removeExtraWhitespace = string => {
-  return string
-    .split("")
-    .map((letter, i) => (string[i + 1] === " " && letter === " " ? "" : letter))
-    .join("")
-    .trim()
-}
+const removeExtraWhitespace = string => string
+  .split('')
+  .map((letter, i) => (string[i + 1] === ' ' && letter === ' ' ? '' : letter))
+  .join('')
+  .trim();
 
-String.prototype.removeExtraWhitespace = function() {
-  return this.split("")
-    .map((letter, i) => (this[i + 1] === " " && letter === " " ? "" : letter))
-    .join("")
-    .trim()
-}
+String.prototype.removeExtraWhitespace = function () {
+  return this.split('')
+    .map((letter, i) => (this[i + 1] === ' ' && letter === ' ' ? '' : letter))
+    .join('')
+    .trim();
+};
 
-String.prototype.kebabCase = function() {
+String.prototype.kebabCase = function () {
   return this.removeExtraWhitespace()
-    .split(" ")
-    .join("-")
-    .toLowerCase()
-}
+    .split(' ')
+    .join('-')
+    .toLowerCase();
+};
 
-String.prototype.snakeCase = function() {
+String.prototype.snakeCase = function () {
   return this.removeExtraWhitespace()
-    .split(" ")
-    .join("_")
-    .toLowerCase()
-}
+    .split(' ')
+    .join('_')
+    .toLowerCase();
+};
 
-String.prototype.camelCase = function() {
+String.prototype.camelCase = function () {
   return this.removeExtraWhitespace()
     .toLowerCase()
     .capitalizeSentence()
-    .split(" ")
-    .join("")
-    .lower()
-}
+    .split(' ')
+    .join('')
+    .lower();
+};
